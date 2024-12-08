bdc75fca2ea7327f271e319a588bbda2b54fc8ab
Va2lyR <tahamedhat135@gmail.com>
2024-12-08 16:47:01 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+clientId: "1311403976071184405", // ايدي البوت
+    guildId: "1173460438671970354", // ايدي السيرفر
+    token: "MTMxMTQwMzk3NjA3MTE4NDQwNQ.GtaCFm.ZXO0WKK1Gw1RJSqSr88UOJlc2jjQ_UmlbjY5XQ", // توكن البوت
+    CommandPermissions: "1173460533064777738" // ايدي الرتبة الى تقدر تستخدم الامر
+}
